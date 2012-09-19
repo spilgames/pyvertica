70ac9bc2a9aa5fc8258148bd2be52b458bd7c43c
Orne Brocaar <info@brocaar.com>
2012-09-19 10:59:27 +0200

    Add _get_random_node_address function.

--- pyvertica/connection.py
+++ pyvertica/connection.py
@@ -24,7 +24,27 @@ def get_connection(dsn, **kwargs):
         See: http://code.google.com/p/pyodbc/wiki/Module#connect
 
     :return:
-        Return of the :py:func:`!pyodbc.connect` function.
+        Return an instance of :class:`!pyodbc.Connection`.
 
     """
     return pyodbc.connect('DSN={0}'.format(dsn), **kwargs)
+
+
+def _get_random_node_address(connection):
+    """
+    Return the address of a random node in the cluster.
+
+    :param connection:
+        An instance of :class:`!pyodbc.Connection`.
+
+    :return:
+        A ``str`` representing the address of the node.
+
+    """
+    cursor = connection.cursor()
+    cursor.execute(
+        'SELECT node_address FROM nodes WHERE node_state = ? '
+        'ORDER BY RANDOM() LIMIT 1',
+        'UP'
+    )
+    return cursor.fetchone().node_address
--- pyvertica/tests/unit/test_connection.py
+++ pyvertica/tests/unit/test_connection.py
@@ -1,8 +1,8 @@
 import unittest2 as unittest
 
-from mock import patch
+from mock import Mock, patch
 
-from pyvertica.connection import get_connection
+from pyvertica.connection import get_connection, _get_random_node_address
 
 
 class ModuleTestCase(unittest.TestCase):
@@ -19,3 +19,22 @@ class ModuleTestCase(unittest.TestCase):
         pyodbc.connect.assert_called_once_with(
             'DSN=VerticaSTG', foo='bar', bar='foo')
         self.assertEqual(pyodbc.connect.return_value, connection)
+
+    def test__get_random_node_address(self):
+        """
+        Test :py:func:`._get_random_node_address`.
+        """
+        connection = Mock()
+        cursor = connection.cursor()
+        row = cursor.fetchone.return_value
+
+        self.assertEqual(
+            row.node_address,
+            _get_random_node_address(connection)
+        )
+
+        cursor.execute.assert_called_once_with(
+            'SELECT node_address FROM nodes WHERE node_state = ? '
+            'ORDER BY RANDOM() LIMIT 1',
+            'UP'
+        )
